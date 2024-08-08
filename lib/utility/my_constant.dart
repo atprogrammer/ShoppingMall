e820584fe491cb20b6c94aa24a69dcb6328a8501
kanoksak <atprogrammer@gmail.com>
2024-08-08 22:34:12 +0700

    update 22:34

---
+++ lib/utility/my_constant.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyConstant {
+  //Genernal
+  static const String appName = 'Shopping Mall';
+
+  //Route
+  static const String rountAuthen = '/authen';
+  static const String rountCreateAccount = '/createAccount';
+  static const String rountBuyerService = 'buyerService';
+  static const String rountSalerService = 'selerService';
+  static const String rountRiderService = 'riderService';
+
+}
